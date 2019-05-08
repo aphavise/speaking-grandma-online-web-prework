5386eaf65e8f7e04ea0e5fda5a0d652f3c621770
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-05-08 04:38:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.capitalize()
-    return "NO, NOT SINCE 1938!"
-  else 
+  elsif phrase == phrase.upcase()
     return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
   end
 end
