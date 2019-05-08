d4117da31eb1d748cde0e606c6ebc923d5d52f73
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-05-08 04:17:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(pharse)
   if pharse == pharse.downcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif pharse == pharse.upcase()
+  elsif pharse != pharse.downcase()
     return "NO, NOT SINCE 1938!"
-  else phrase == " I LOVE YOU GRANDMA!"
+  else phrase = " I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
