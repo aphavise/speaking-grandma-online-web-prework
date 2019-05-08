756168ea4bbe892fbc13c20f4fdddfd1cddb22f8
reliable-caller-8794 <andrewphaviseth@gmail.com>
2019-05-08 03:53:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,3 +19,11 @@ def speak_to_grandma(pharse)
     puts "NO, NOT SINCE 1938!.
   end
 end
+
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
